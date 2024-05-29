d434159bec4a11b8232f5b610b61a7b6c3bb2fe8
Edward <eddy.the.professor@gmail.com>
2024-05-28 20:44:02 -0400

    Added lists assignment (pseudocode pending)

---
+++ ICS3U1/List_Assignment/penny_pitch.py
@@ -0,0 +1,214 @@
+'''
+Name: Edward Drobnis
+Date: May 22, 2024
+Title: Penny Pitch
+Description: Creates a board that consists of 5 x 5 squares, with 15 containing prizes
+'''
+# Imports the random library
+import random
+def select_prizes(prizes, squares):
+    '''
+    Returns a list of all prizes in random squares
+    :param prizes:
+    :param squares:
+    :return:
+    '''
+    # Loops through each prize in the prizes list until the count of the prize is 0
+    for prize, count in prizes:
+        while count > 0:
+            # Randomly generates a row and column
+            row = random.randint(0, 4)
+            col = random.randint(0, 4)
+            # If the square is blank, change the square to the prize's name and reduce the count of the prize by 1
+            if squares[row][col] == "":
+                squares[row][col] = prize
+                count -= 1
+
+def draw_squares(squares):
+    '''
+    Displays a board
+    :param squares:
+    :return:
+    '''
+    print()
+    # Loops through each row in the squares list and prints horizontal lines
+    for row in squares:
+        print("-" * 86)
+        # Loops through each tile in the row and prints the prize's name (or an empty square)
+        for i in range(5):
+            print("|{:^16}".format(row[i]), end="")
+        print("|")
+    print("-" * 86)
+    print()
+
+def penny_pitch(prizes, squares):
+    '''
+    Randomly selected a tile to drop a penny
+    :param prizes:
+    :param squares:
+    :return:
+    '''
+    # Selects a random row and column
+    row = random.randint(0, 4)
+    col = random.randint(0, 4)
+
+    # Checks if the selected row and column contains a prize
+    for i in range(len(prizes)):
+        if prizes[i][0] == squares[row][col]:
+            # If so, reduce the count of that prize by 1
+            prizes[i][1] -= 1
+
+    # Add an asterisk to the tile (indicating that it is hit)
+    squares[row][col] += "*"
+
+def display_prizes_won(prizes):
+    '''
+    Displays all the prizes won
+    :param prizes:
+    :return:
+    '''
+    # Creates an empty list that will contain all the prizes won
+    prizes_won = []
+    # Loops through all the prizes and checks if the count is 0
+    for prize, count in prizes:
+        if count == 0:
+            # If so, add the prize's name to prizes_won
+            prizes_won.append(prize)
+
+    # If no prizes have a final count of 0, print "No prizes won!"
+    if not prizes_won:
+        print("No prizes won!")
+
+    # Otherwise, loop through the list of prizes won and print each value, separated by commas
+    else:
+        print(f"Item(s) won:", end = " ")
+        for i in range(len(prizes_won)):
+            print(prizes_won[i], end = "")
+            if i != len(prizes_won) - 1:
+                print(",", end = " ")
+
+def main():
+    '''
+    Main function that initializes the prizes and calls the necessary functions
+    :return:
+    '''
+    # Creates a 2D list containing the prize name and count
+    prizes = [["Puzzle", 3], ["Game", 3], ["Ball", 3], ["Poster", 3], ["Doll", 3]]
+    # Creates a 2D list containing 5x5 empty squares (5 elements per list and a total of 5 lists in the list)
+    squares = [[""] * 5 for _ in range(5)]
+    print("New Board")
+    select_prizes(prizes, squares)
+    draw_squares(squares)
+
+    a = input("Enter any key and enter to continue: ")
+
+    # Calls the penny_pitch function 10 times
+    for _ in range(10):
+        penny_pitch(prizes, squares)
+
+    print("\nNew Board With Pennies Tossed")
+    draw_squares(squares)
+
+    display_prizes_won(prizes)
+
+run = 0
+while True:
+    while run > 0:
+        print()
+        # If the program is run over 1 time, display an option to stop the program or continue to a new game
+        try:
+            r = int(input("Enter \"0\" to stop or any other integer to continue: "))
+            break
+        except ValueError:
+            print("Enter \"0\" to stop or any other integer to continue!")
+    if run == 0 or r != 0:
+        main()
+        run += 1
+    else:
+        break
+
+'''
+Pseudo Code:
+
+import random
+def select_prizes(prizes, squares):
+    for prize, count in prizes:
+        while count > 0:
+            row = random.randint(0, 4)
+            col = random.randint(0, 4)
+            if squares[row][col] == "":
+                squares[row][col] = prize
+                count -= 1
+
+def draw_squares(squares):
+    print()
+    for row in squares:
+        print("-" * 86)
+        for i in range(5):
+            print("|{:^16}".format(row[i]), end="")
+        print("|")
+    print("-" * 86)
+    print()
+
+def penny_pitch(prizes, squares):
+    row = random.randint(0, 4)
+    col = random.randint(0, 4)
+
+    while squares[row][col] == "x":
+        row = random.randint(0, 4)
+        col = random.randint(0, 4)
+
+    for i in range(len(prizes)):
+        if prizes[i][0] == squares[row][col]:
+            prizes[i][1] -= 1
+
+    squares[row][col] += "*"
+
+def display_prizes_won(prizes):
+    prizes_won = []
+    for prize, count in prizes:
+        if count == 0:
+            prizes_won.append(prize)
+
+    if not prizes_won:
+        print("No prizes won!")
+
+    else:
+        print(f"Item(s) won:", end = " ")
+        for i in range(len(prizes_won)):
+            print(prizes_won[i], end = "")
+            if i != len(prizes_won) - 1:
+                print(",", end = " ")
+
+def main():
+    prizes = [["Puzzle", 3], ["Game", 3], ["Ball", 3], ["Poster", 3], ["Doll", 3]]
+    squares = [[""] * 5 for _ in range(5)]
+    print("New Board")
+    select_prizes(prizes, squares)
+    draw_squares(squares)
+
+    a = input("Enter any key and enter to continue: ")
+
+    for _ in range(10):
+        penny_pitch(prizes, squares)
+
+    print("\nNew Board With Pennies Tossed")
+    draw_squares(squares)
+
+    display_prizes_won(prizes)
+
+run = 0
+while True:
+    while run > 0:
+        print()
+        try:
+            r = int(input("Enter \"0\" to stop or any other integer to continue: "))
+            break
+        except ValueError:
+            print("Enter \"0\" to stop or any other integer to continue!")
+    if run == 0 or r != 0:
+        main()
+        run += 1
+    else:
+        break
+'''
---
+++ ICS3U1/List_Assignment/sorted_grades.py
@@ -0,0 +1,196 @@
+'''
+Name: Edward Drobnis
+Date: May 21, 2024
+Title: Sorted Grades
+Description: Prompts users to enter grades, delete grades, see all grades, and see the lowest and highest grades
+'''
+
+# Creates a list containing 10 zeros
+grades = list(0 for i in range(10))
+
+def sort_grades():
+    '''
+    Function to sort graded in descending order
+    :return:
+    '''
+    # Loops through each grade in grades (except for the last one)
+    for i in range(len(grades) - 1):
+        # Loops through each grade in grades (starts at i + 1, where i is the outer loop iteration)
+        for j in range(i + 1, len(grades)):
+            # Compares two grades and checks if the one on the left is smaller than the one on the right
+            if grades[i] < grades[j]:
+                # If so, swap the values of those two grades
+                grades[i], grades[j] = grades[j], grades[i]
+def add_grade(grade):
+    '''
+    Function to add grade to the grades list and sort it in descending order
+    :param grade:
+    :return: "Successfully added {grade}% to the list" or "List is full. Please remove a grade and try again."
+    '''
+    # Loops through each element in grades
+    for i in range(len(grades)):
+        # If the current grade is 0, it gets deleted, the user-inputted grade is added, and the grades are sorted
+        if grades[i] == 0:
+            del grades[i]
+            grades.append(grade)
+            sort_grades()
+            return print("Successfully added {}% to the list\n".format(grade))
+    # If no zeros are found in the grades list, it states to remove a grade first
+    return print("List is full. Please remove a grade and try again.\n")
+
+def delete_grade(grade):
+    '''
+    Function to remove a grade from grades
+    :param grade:
+    :return:
+    '''
+    # Checks if the highest number in grades is 0,
+    # If true, it will return "No grades were entered"
+    if max(grades) == 0:
+        return print("No grades were entered. Please enter a grade and try again.\n")
+
+    # Otherwise, it will loop through each grade until it finds the user-inputted grade
+    for i in range(len(grades)):
+        # If it finds the user-inputted grade, it removes it from the list and adds a 0 to the end of the list
+        if grade == grades[i]:
+            del grades[i]
+            grades.append(0)
+            return print("Successfully deleted {}% from the list\n".format(grade))
+    # If the grade was not found, return "Grade was not found"
+    return print("Grade was not found.\n")
+
+def lowest_grade():
+    '''
+    Function to return the lowest grade in the grades list (other than 0)
+    :return: "No grades entered" or lowest grade
+    '''
+    # Loops through the grades list, starting from the end of the loop
+    for i in range(-1, -len(grades), -1):
+        # If the current value is not 0, return the current value
+        if grades[i] != 0:
+            return print("The lowest grade is: {}%\n".format(grades[i]))
+    # If all the values are 0, return "No grades entered!"
+    return print("No grades entered!\n")
+
+def highest_grade():
+    '''
+    Function to return the highest grade in the grades list (other than 0)
+    :return:
+    '''
+    # Loops through the grades list
+    for i in range(len(grades)):
+        # If the current value is not 0, return the current grade as the highest value
+        if grades[i] != 0:
+            return print("The highest grade is: {}%\n".format(grades[i]))
+    # If all the grades are 0, return "No grades entered!"
+    return print("No grades entered!\n")
+
+def show_grades():
+    '''
+    Function to display the grades entered
+    :return:
+    '''
+    # Prints horizontal lines
+    print("-" * ((8 * len(grades)) - (len(grades) - 1)))
+    # Loops through the length of grades and adds the indexes, separated by |
+    for i in range(len(grades)):
+        print("|{:>5}".format(i), end=" ")
+    # Prints |, followed by horizontal lines on the next line
+    print("|")
+    print("-" * ((8 * len(grades)) - (len(grades) - 1)))
+
+    # Loops through each grade and prints them, separated by |
+    for i in range(len(grades)):
+        print("|{:>5}".format(grades[i]), end=" ")
+
+    # Prints |, followed by horizontal lines on the next line
+    print("|")
+    print("-" * ((8 * len(grades)) - (len(grades) - 1)))
+    return print("\n")
+
+def clear_grades():
+    '''
+    Function to clear the grades
+    :return:
+    '''
+    # Checks if the highest number is 0
+    # If so, the grades are already cleared
+    if grades[0] == 0:
+        return print("Grades already cleared!\n")
+
+    # Otherwise it will loop through each grade and change the value to 0
+    else:
+        for i in range(len(grades)):
+            grades[i] = 0
+        return print("Cleared grades!\n")
+
+
+def main():
+    '''
+    Function to display menu
+    :return:
+    '''
+    while True:
+        print("Choose from the following menu:")
+        print("0. Enter a grade")
+        print("1. Delete a grade")
+        print("2. Show grades")
+        print("3. Display lowest grade")
+        print("4. Display highest grade")
+        print("5. Clear list")
+        print("6. Quit")
+
+        try:
+            option = int(input("\nEnter option (0-6): "))
+            if option == 0:
+                while True:
+                    try:
+                        grade = int(input("Enter a grade (between 1 and 100): "))
+                        if 0 < grade and grade <= 100:
+                            add_grade(grade)
+                            break
+                        else:
+                            raise ValueError
+                    except ValueError:
+                        print("Grade must be an integer between 1 and 100")
+                        continue
+
+            elif option == 1:
+                while True:
+                    try:
+                        grade = int(input("Enter a grade (between 1 and 100): "))
+                        if 0 < grade and grade <= 100:
+                            delete_grade(grade)
+                            break
+                        else:
+                            raise ValueError
+                    except ValueError:
+                        print("Grade must be an integer between 1 and 100")
+                        continue
+
+            elif option == 2:
+                show_grades()
+
+            elif option == 3:
+                lowest_grade()
+
+            elif option == 4:
+                highest_grade()
+
+            elif option == 5:
+                clear_grades()
+
+            elif option == 6:
+                exit()
+
+            else:
+                print("Invalid option. Choose option 0 to 6.\n")
+                continue
+
+        except ValueError:
+            print("Invalid option. Choose option 0 to 6.\n")
+            continue
+
+while True:
+    main()
+
